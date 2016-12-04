8e1b512fdba41a3d4c678a9bf377e95b9da81cc0
digitalartcrew <digitalartcrew@gmail.com>
2016-12-04 11:37:46 -0600

    Initial Setup

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {NavigatorIOS, StyleSheet, AppRegistry} from 'react-native';
 
-export default class FarmersMarketFinder extends Component {
+let SearchPage = require('./SearchPage');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+class FarmersMarketFinderApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'FarmersMarketFinder',
+          component: SearchPage,
+        }}/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('FarmersMarketFinder', () => FarmersMarketFinder);
+AppRegistry.registerComponent('FarmersMarketFinder', function() { return FarmersMarketFinderApp });
